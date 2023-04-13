cc65c422d81fac0995a692c43a039468c9ce4cb8
Chance Gardner <Chantz465@gmail.com>
2023-04-13 16:16:19 -0600

    added more to test git with to see if I can upload code again

---
+++ HelloTest/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hello all the people of git hub is now showing up and letting me upload my code or is it still unhappy");
